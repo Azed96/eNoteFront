4ec93f938f461b01b5f5d5c9909927fca231e96b
Votre nom ou pseudo <azzeddine.bouda@gmail.com>
2020-11-02 02:15:07 +0100

    Style list of filiere

--- src/views/examples/Filiere/ListeFiliereComponent.js
+++ src/views/examples/Filiere/ListeFiliereComponent.js
@@ -1,69 +1,108 @@
-import React ,{Component} from 'react';
+import React, { Component } from 'react';
 import AdminService from '../../../_services/AdminService';
+import Header from "../../../components/Headers/Header";
+import {
+    Card, Table, Container, Row, CardHeader,
+    Button
+} from "reactstrap";
 
-class ListeFiliereComponent extends Component{
-    constructor(props){
+class ListeFiliereComponent extends Component {
+    constructor(props) {
         super(props);
-        this.state={ 
-            filieres:[]
+        this.state = {
+            filieres: []
         }
-       this.addFiliere=this.addFiliere.bind(this);
-        this.editFiliere=this.editFiliere.bind(this);
-         this.deleteFiliere=this.deleteFiliere.bind(this);
+        this.addFiliere = this.addFiliere.bind(this);
+        this.editFiliere = this.editFiliere.bind(this);
+        this.deleteFiliere = this.deleteFiliere.bind(this);
     }
 
-    componentDidMount(){
-        AdminService.getAllFiliere().then((response)=>{
-            this.setState({filieres: response});
+    componentDidMount() {
+        AdminService.getAllFiliere().then((response) => {
+            this.setState({ filieres: response });
         });
-        console.log("filieres ="+ this.state.filieres);
+        console.log("filieres =" + this.state.filieres);
     }
 
-    addFiliere(){
+    addFiliere() {
         this.props.history.push(`/administrateur/add-update-filiere/-addFiliere`);
     }
-    editFiliere(id){
+    editFiliere(id) {
         this.props.history.push(`/administrateur/add-update-filiere/${id}`);
     }
-    viewFiliere(id){
+    viewFiliere(id) {
         this.props.history.push(`/administrateur/ViewFiliereComponent/${id}`);
     }
 
-    deleteFiliere(id){
-        AdminService.deleteFiliere(id).then(reponse=>{
-            this.setState({filieres: this.state.filieres.filter(filiere=>filiere.id!=id)});
+    deleteFiliere(id) {
+        AdminService.deleteFiliere(id).then(reponse => {
+            this.setState({ filieres: this.state.filieres.filter(filiere => filiere.id != id) });
         })
     }
- 
-    render(){
-        return( 
-        <div>
-            <h1 className= "text-center" > Liste filiere</h1>
-            <div className="row">
-                <button className="btn btn-primary" onClick={this.addFiliere}>Add Filiere</button>
-            </div>
-            <table className= " table table-striped" >
-                <thead>
-                    <tr>
-                        <th>nom</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.filieres.map(
-                            filiere=>
-                            <tr key ={filiere.id}>
-                                <td>{filiere.nom}</td>
-                                <button onClick ={()=> this.editFiliere(filiere.id)} className="btn btn-info">Update</button>
-                                <button style={{marginLeft:"10px"}} onClick ={()=> this.deleteFiliere(filiere.id)} className="btn btn-danger">Delete</button>
-                                <button style={{marginLeft:"10px"}} onClick ={()=> this.viewFiliere(filiere.id)} className="btn btn-info">View</button>
-                            </tr>
-                        )
-                    }
-                </tbody>
-            </table>
-        </div>
+
+    render() {
+        return (
+            <>
+                <Header />
+                <Container className="mt--7" fluid>
+                    <Row>
+                        <div className="col">
+                            <Card className="shadow">
+                                <CardHeader className="border-0">
+                                    <h3 className="mb-0 text-center"> Liste des Filières  </h3>
+                                </CardHeader>
+                                <Table className="align-items-center table-flush" responsive>
+                                    <thead className="thead-light">
+                                        <tr>
+                                            <th scope="col">Nom</th>
+                                            <th scope="col">Année Universitaire</th>
+                                            <th scope="col">Action</th>
+                                        </tr>
+
+                                    </thead>
+                                    <tbody>
+
+                                        {
+                                            this.state.filieres.map(
+                                                filiere =>
+                                                    <tr key={filiere.id}>
+                                                        <td>{filiere.nom}</td>
+                                                        <td>2020/2021</td>
+                                                        <td>
+                                                            <Button
+                                                                color="info"
+                                                                className="text-center"
+                                                                onClick={() => this.editFiliere(filiere.id)}
+                                                            >
+                                                                Mettre à jour
+                                                            </Button>
+                                                            <Button
+                                                                color="danger"
+                                                                onClick={() => this.deleteFiliere(filiere.id)}
+                                                            >
+                                                                Supprimer
+                                                            </Button>
+                                                            <Button
+                                                                color="primary"
+                                                                onClick={() => this.viewFiliere(filiere.id)}                                                                >
+                                                                Consulter
+                                                            </Button>
+                                                        </td>
+
+
+                                                    </tr>
+                                            )
+                                        }
+
+                                    </tbody>
+                                </Table>
+                            </Card>
+                        </div>
+                    </Row>
+                   
+                </Container>
+
+            </>
         )
     }
 }
